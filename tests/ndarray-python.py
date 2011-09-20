a994d90b5a924bed14cffc768551cbbfb6b1b256
pgee <pgee@git.lsstcorp.org>
2011-09-20 21:54:17 +0000

    Added tests to PyConverters to be sure that numpy.array and dtype checking is done correctly

--- tests/ndarray-python.py
+++ tests/ndarray-python.py
@@ -6,6 +6,46 @@ import unittest
 
 class TestNDArray(unittest.TestCase):
 
+    def testIntArrayConversion(self):
+        a1 = numpy.zeros((5,3,4),dtype=numpy.int32)
+        a2 = a1[:,:,:2]
+        self.assert_(a1.flags["WRITEABLE"])
+        self.assert_(a2.flags["WRITEABLE"])
+
+        b1 = ndarray_python_test.passIntArray33(a1)
+        self.assert_(b1.flags["WRITEABLE"])
+        self.assertEqual(a1.shape,b1.shape)
+        self.assertEqual(a1.strides,b1.strides)
+
+
+        c1 = ndarray_python_test.passIntArray30(a1)
+        self.assert_(c1.flags["WRITEABLE"])
+        self.assertEqual(a1.shape,c1.shape)
+        self.assertEqual(a1.strides,c1.strides)
+
+        d1 = ndarray_python_test.passConstIntArray33(a1)
+        self.assertEqual(a1.shape,d1.shape)
+        self.assertEqual(a1.strides,d1.strides)
+        self.assert_(not d1.flags.writeable)
+
+        self.assertRaises(ValueError, ndarray_python_test.passIntArray33, a2)
+
+        c2 = ndarray_python_test.passIntArray30(a2)
+        self.assert_(c2.flags["WRITEABLE"])
+        self.assertEqual(a2.shape,c2.shape)
+        self.assertEqual(a2.strides,c2.strides)
+
+        d2 = ndarray_python_test.passConstIntArray33(a2)
+        self.assertEqual(a2.shape,d2.shape)
+        self.assertNotEqual(a2.strides,d2.strides)
+        self.assert_(not d2.flags.writeable)
+
+        # test to be sure a tuple of tuples does not work
+        self.assertRaises(ValueError, ndarray_python_test.passIntArray30, ((1,1,1),(2,2,2),(3,3,3)))
+
+        # test to be sure an array of the wrong type does not work
+        self.assertRaises(ValueError, ndarray_python_test.passIntArray30, numpy.zeros((4,3,4),dtype=numpy.float))
+
     def testVectorConversion(self):
         a = (3.5,6.7,1.2)
         b = ndarray_python_test.passFloatVector3(a)
@@ -21,6 +61,7 @@ class TestNDArray(unittest.TestCase):
         self.assert_(b1.flags["WRITEABLE"])
         self.assertEqual(a1.shape,b1.shape)
         self.assertEqual(a1.strides,b1.strides)
+
 
         c1 = ndarray_python_test.passFloatArray30(a1)
         self.assert_(c1.flags["WRITEABLE"])
@@ -44,8 +85,17 @@ class TestNDArray(unittest.TestCase):
         self.assertNotEqual(a2.strides,d2.strides)
         self.assert_(not d2.flags.writeable)
 
-    def testArrayCreation(self):
+        # test to be sure a tuple of tuples does not work
+        self.assertRaises(ValueError, ndarray_python_test.passFloatArray30, ((1.0,1.0,1.0),(2.0,2.0,2.0),(3.0,3.0,3.0)))
+
+        # test to be sure an array of the wrong type does not work
+        self.assertRaises(ValueError, ndarray_python_test.passFloatArray30, numpy.zeros((4,3,4),dtype=numpy.int32))
+
+    def testFloatArrayCreation(self):
         a = ndarray_python_test.makeFloatArray3((3,4,5))
+    
+    def testIntArrayCreation(self):
+        a = ndarray_python_test.makeIntArray3((3,4,5))
 
 class TestNumpySwig(unittest.TestCase):
 
