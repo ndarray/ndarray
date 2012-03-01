6ff81a7744102a6b1ef1805e47f084f24a5cace1
Jim Bosch <jbosch@astro.princeton.edu>
2012-03-01 01:17:38 -0500

    rearranging old boost/python/ndarray library for re-inclusion into ndarray proper

--- boost/python/ndarray/tables.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef BOOST_PYTHON_NDARRAY_TABLES_HPP_INCLUDED
-#define BOOST_PYTHON_NDARRAY_TABLES_HPP_INCLUDED
-
-#include <boost/python/ndarray/tables/Layout.hpp>
-#include <boost/python/ndarray/tables/Table.hpp>
-
-#endif // !BOOST_AUGMENTED_PYTHON_NDARRAY_TABLES_HPP_INCLUDED
