46df66f0e6eec7ec7b7b86878aee5407d312a08b
Jim Bosch <jbosch@astro.princeton.edu>
2016-07-15 16:46:39 -0400

    Add test for default-constructed Arrays.

--- tests/Array.cpp
+++ tests/Array.cpp
@@ -5,3 +5,19 @@
 
 namespace ndd = ndarray::detail;
 namespace nd = ndarray;
+
+TEST_CASE( "default-constructed Arrays behave appropriately", "[array-default-ctor]" ) {
+    nd::Array<float,3> empty;
+    REQUIRE( empty.data() == nullptr );
+    REQUIRE( empty.manager() == nullptr );
+    REQUIRE( empty.stride() == 0 );
+    REQUIRE( empty.size() == 0);
+    REQUIRE( empty.full_size() == 0);
+    REQUIRE( empty.stride<0>() == 0 );
+    REQUIRE( empty.size<0>() == 0);
+    REQUIRE( empty.stride<1>() == 0 );
+    REQUIRE( empty.size<1>() == 0);
+    REQUIRE( empty.stride<2>() == 0 );
+    REQUIRE( empty.size<2>() == 0);
+    REQUIRE( std::distance(empty.begin(), empty.end()) == 0 );
+}
