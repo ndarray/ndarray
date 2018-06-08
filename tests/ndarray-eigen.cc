1c769166ad14f771e76ccdee6cce97912e7716a6
Jim Bosch <jbosch@astro.princeton.edu>
2018-06-08 17:55:58 -0400

    Add new asEigen* free functions returning Eigen::Map.
    These should now be used instead of Array.asEigen().

--- tests/ndarray-eigen.cc
+++ tests/ndarray-eigen.cc
@@ -23,7 +23,7 @@ void testElements2(T const & a, U const & b) {
     BOOST_CHECK( a.template getStride<1>() == b.colStride() );
     for (int i = 0; i < b.rows(); ++i) {
         for (int j = 0; j < b.cols(); ++j) {
-            BOOST_CHECK(&a[i][j] == &b(i,j));
+            BOOST_CHECK(&a[i][j] == &b.coeffRef(i,j));
         }
     }    
 }
@@ -33,8 +33,96 @@ void testElements1(T const & a, U const & b) {
     BOOST_CHECK( a.template getSize<0>() == b.size() );
     BOOST_CHECK( a.template getStride<0>() == b.innerStride() );
     for (int i = 0; i < b.size(); ++i) {
-        BOOST_CHECK(&a[i] == &b[i]);
+        BOOST_CHECK(&a[i] == &b.coeffRef(i));
     }
+}
+
+template <typename T, int N, int C>
+ndarray::Array<T, N, C> makeMutable(ndarray::Array<T, N, C> const & a) {
+    return a;
+}
+
+template <typename T, int N, int C>
+ndarray::ArrayRef<T, N, C> makeMutable(ndarray::ArrayRef<T, N, C> const & a) {
+    return a;
+}
+
+template <typename T, int N, int C>
+ndarray::Array<T, N, C> makeMutable(ndarray::Array<T const, N, C> const & a) {
+    return ndarray::const_array_cast<T>(a);
+}
+
+template <typename T, int N, int C>
+ndarray::ArrayRef<T, N, C> makeMutable(ndarray::ArrayRef<T const, N, C> const & a) {
+    return ndarray::const_array_cast<T>(a.shallow()).deep();
+}
+
+template <int Rows, typename XprKind, typename A>
+void testAsEigen1(A const & a) {
+    typedef typename boost::remove_const<typename A::Element>::type T;
+    ndarray::asEigen<XprKind>(makeMutable(a)).setRandom();
+    int const n = asEigenMatrix(a).size();
+    testElements1(a, ndarray::asEigen<XprKind>(a));
+    Eigen::Matrix<T, Rows, Eigen::Dynamic> m1(n, 6);
+    m1.setRandom(n, 6);
+    Eigen::Matrix<T, Eigen::Dynamic, 1> m2(6, 1);
+    m2.setRandom(6, 1);
+    asEigenMatrix(makeMutable(a)) = m1 * m2;
+    Eigen::Matrix<T, Rows, 1> m3 = m1 * m2;
+    for (int i = 0; i < n; ++i) {
+        BOOST_CHECK(a[i] == m3(i));
+    }
+    Eigen::Array<T, Rows, 1> m4(n, 1);
+    m4.setRandom();
+    Eigen::Array<T, Rows, 1> m5 = m4 * asEigenArray(a);
+    Eigen::Array<T, Rows, 1> m6 = m4 * m3.array();
+    BOOST_CHECK((m5 == m6).all());
+}
+
+template <int Rows, int Cols, typename XprKind, typename A>
+void testAsEigen2(A const & a) {
+    typedef typename boost::remove_const<typename A::Element>::type T;
+    ndarray::asEigen<XprKind>(makeMutable(a)).setRandom();
+    int const m = asEigenMatrix(a).rows();
+    int const n = asEigenMatrix(a).cols();
+    testElements2(a, ndarray::asEigen<XprKind>(a));
+    Eigen::Matrix<T, Rows, Eigen::Dynamic> m1(m, 6);
+    m1.setRandom(m, 6);
+    Eigen::Matrix<T, Eigen::Dynamic, Cols> m2(6, n);
+    m2.setRandom(6, n);
+    asEigenMatrix(makeMutable(a)) = m1 * m2;
+    Eigen::Matrix<T, Rows, Cols> m3 = m1 * m2;
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            BOOST_CHECK(a[i][j] == m3(i, j));
+        }
+    }
+    Eigen::Array<T, Rows, Cols> m4(m, n);
+    m4.setRandom();
+    Eigen::Array<T, Rows, Cols> m5 = m4 * asEigenArray(a);
+    Eigen::Array<T, Rows, Cols> m6 = m4 * m3.array();
+    BOOST_CHECK((m5 == m6).all());
+}
+
+template <typename T, typename XprKind>
+void invokeAsEigenTests() {
+    ndarray::Array<T, 2, 2> a22(ndarray::allocate(5, 4));
+    testAsEigen2<5, 4, XprKind>(a22);
+
+    ndarray::Array<T, 2, 1> a21(a22[ndarray::view()(0, 3)]);
+    testAsEigen2<5, 3, XprKind>(a21);
+    testAsEigen2<3, 5, XprKind>(a21.transpose());
+
+    ndarray::Array<T, 2, 0> a20(a22[ndarray::view()(0, 4, 2)]);
+    testAsEigen2<5, 2, XprKind>(a20);
+    testAsEigen2<2, 5, XprKind>(a20.transpose());
+
+    ndarray::Array<T, 1, 1> a11(ndarray::allocate(4));
+    testAsEigen1<4, XprKind>(a11);
+    testAsEigen1<4, XprKind>(a11.transpose());
+    ndarray::Array<T, 1, 0> a10(a11[ndarray::view(0, 4, 2)]);
+    testAsEigen1<2, XprKind>(a10);
+    testAsEigen1<2, XprKind>(a10.transpose());
 }
 
 template <int C, int Rows, int Cols>
@@ -156,6 +244,13 @@ void invokeEigenViewTests() {
     testEigenView(a10.transpose().asEigen<XprKind>());
     testEigenView(a10.transpose().asEigen<XprKind,2,1>());
     testEigenView(a10.transpose().asEigen<XprKind,1,2>());
+}
+
+BOOST_AUTO_TEST_CASE(AsEigen) {
+    invokeAsEigenTests<double, Eigen::ArrayXpr>();
+    invokeAsEigenTests<double, Eigen::MatrixXpr>();
+    invokeAsEigenTests<float const, Eigen::ArrayXpr>();
+    invokeAsEigenTests<float const, Eigen::MatrixXpr>();
 }
 
 BOOST_AUTO_TEST_CASE(EigenView) {
