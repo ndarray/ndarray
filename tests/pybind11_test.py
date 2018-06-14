d4a64866aab40bf295ed2712f165bfce153c40e6
Russell Owen <rowen@uw.edu>
2018-06-14 14:28:17 -0700

    Modernize pybind11_test.py

--- tests/pybind11_test.py
+++ tests/pybind11_test.py
@@ -9,54 +9,56 @@
 #  https://github.com/ndarray/ndarray
 #
 import numpy
-import pybind11_test_mod
 import unittest
+
+import pybind11_test_mod
+
 
 class TestNumpyPybind11(unittest.TestCase):
 
     def testMatrixXd(self):
         m1 = pybind11_test_mod.returnMatrixXd()
-        m2 = numpy.matrix(numpy.arange(15, dtype=float).reshape(3,5).transpose())
-        self.assert_((m1 == m2).all())
-        self.assert_(pybind11_test_mod.acceptMatrixXd(m2))
+        m2 = numpy.matrix(numpy.arange(15, dtype=float).reshape(3, 5).transpose())
+        self.assertTrue((m1 == m2).all())
+        self.assertTrue(pybind11_test_mod.acceptMatrixXd(m2))
 
     def testMatrix2d(self):
         m1 = pybind11_test_mod.returnMatrix2d()
         m2 = numpy.matrix([[0.0, 2.0], [1.0, 3.0]])
-        self.assert_((m1 == m2).all())
-        self.assert_(pybind11_test_mod.acceptMatrix2d(m2))
+        self.assertTrue((m1 == m2).all())
+        self.assertTrue(pybind11_test_mod.acceptMatrix2d(m2))
 
     def testArray1(self):
         a1 = pybind11_test_mod.returnArray1()
         a2 = numpy.arange(6, dtype=float)
-        self.assert_((a1 == a2).all())
-        self.assert_(pybind11_test_mod.acceptArray1(a2))
+        self.assertTrue((a1 == a2).all())
+        self.assertTrue(pybind11_test_mod.acceptArray1(a2))
         a3 = pybind11_test_mod.returnConstArray1()
-        self.assert_((a1 == a3).all())
-        self.assert_(a3.flags["WRITEABLE"] == False)
+        self.assertTrue((a1 == a3).all())
+        self.assertFalse(a3.flags["WRITEABLE"])
 
     def testArray3(self):
         a1 = pybind11_test_mod.returnArray3()
-        a2 = numpy.arange(4*3*2, dtype=float).reshape(4,3,2)
-        self.assert_((a1 == a2).all())
-        self.assert_(pybind11_test_mod.acceptArray3(a2))
+        a2 = numpy.arange(4*3*2, dtype=float).reshape(4, 3, 2)
+        self.assertTrue((a1 == a2).all())
+        self.assertTrue(pybind11_test_mod.acceptArray3(a2))
         a3 = pybind11_test_mod.returnConstArray3()
-        self.assert_((a1 == a3).all())
-        self.assert_(a3.flags["WRITEABLE"] == False)
+        self.assertTrue((a1 == a3).all())
+        self.assertFalse(a3.flags["WRITEABLE"])
 
     def testClass(self):
         a = pybind11_test_mod.MatrixOwner()
         m1 = a.member
         m2 = a.getMember()
-        self.assert_((m1 == 0).all())
-        self.assert_((m2 == 0).all())
-        self.assertEqual(m1.shape, (2,2))
-        self.assertEqual(m2.shape, (2,2))
+        self.assertTrue((m1 == 0).all())
+        self.assertTrue((m2 == 0).all())
+        self.assertEqual(m1.shape, (2, 2))
+        self.assertEqual(m2.shape, (2, 2))
 
     def testOverloads(self):
         self.assertEqual(pybind11_test_mod.acceptOverload(1), 0)
-        self.assertEqual(pybind11_test_mod.acceptOverload(numpy.zeros((2,2), dtype=float)), 2)
-        self.assertEqual(pybind11_test_mod.acceptOverload(numpy.zeros((3,3), dtype=float)), 3)
+        self.assertEqual(pybind11_test_mod.acceptOverload(numpy.zeros((2, 2), dtype=float)), 2)
+        self.assertEqual(pybind11_test_mod.acceptOverload(numpy.zeros((3, 3), dtype=float)), 3)
 
     def testStrideHandling(self):
         # in NumPy 1.8+ 1- and 0-sized arrays can have arbitrary strides; we should
@@ -91,9 +93,9 @@ class TestNumpyPybind11(unittest.TestCase):
 
     def testFullySpecifiedMatrix(self):
         # Failure on this specific case was not caught by other unit tests
-        a = numpy.array([[ 1.,  0.], [ 0.,  1.]])
-        b = numpy.array([ 0.,  0.])
-        self.assert_(pybind11_test_mod.acceptFullySpecifiedMatrix(a, b))
+        a = numpy.array([[1., 0.], [0., 1.]])
+        b = numpy.array([0., 0.])
+        self.assertTrue(pybind11_test_mod.acceptFullySpecifiedMatrix(a, b))
 
     def testNonNativeByteOrder(self):
         d1 = numpy.dtype("<f8")
@@ -101,6 +103,7 @@ class TestNumpyPybind11(unittest.TestCase):
         nonnative = d2 if d1 == numpy.dtype(float) else d1
         a = numpy.zeros(5, dtype=nonnative)
         self.assertRaises(TypeError, pybind11_test_mod.acceptArray10, a)
+
 
 if __name__ == "__main__":
     unittest.main()
